5e557ad94aedb6a146c9e553fb790bd723d1b4cd
Rafael Borges <rafaelfborges@gmail.com>
2020-06-17 13:33:07 -0300

    Create project with Spring Boot at 2.3.1

---
+++ src/main/java/dev/codenation/errorcentral/ErrorCentralApplication.java
@@ -0,0 +1,13 @@
+package dev.codenation.errorcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorCentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/codenation/errorcentral/ErrorCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.codenation.errorcentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
