eeba1b508ec05d9ca624181762497aa63e99fc1b
Rafael Borges <rafaelfborges@gmail.com>
2020-06-21 14:38:10 -0300

    Changes in event repository and service to findById

--- src/main/java/dev/codenation/errorcentral/repository/EventRepository.java
+++ src/main/java/dev/codenation/errorcentral/repository/EventRepository.java
@@ -2,7 +2,9 @@ package dev.codenation.errorcentral.repository;
 
 import dev.codenation.errorcentral.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
 }
--- src/main/java/dev/codenation/errorcentral/service/impl/EventService.java
+++ src/main/java/dev/codenation/errorcentral/service/impl/EventService.java
@@ -3,6 +3,7 @@ package dev.codenation.errorcentral.service.impl;
 import dev.codenation.errorcentral.entity.Event;
 import dev.codenation.errorcentral.repository.EventRepository;
 import dev.codenation.errorcentral.service.interfaces.EventServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,10 +11,11 @@ import java.util.Optional;
 @Service
 public class EventService implements EventServiceInterface {
 
+    @Autowired
     private EventRepository eventRepository;
 
     @Override
     public Optional<Event> findById(Long eventId) {
-        return Optional.empty();
+        return eventRepository.findById(eventId);
     }
 }
