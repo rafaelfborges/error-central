7e76ef1bc8861d51bf2dbd30dca15567fbfafc8a
Rafael Borges <rafaelfborges@gmail.com>
2020-06-23 11:50:15 -0300

    Changes URN and findById methods

--- src/main/java/dev/codenation/errorcentral/endpoints/EventController.java
+++ src/main/java/dev/codenation/errorcentral/endpoints/EventController.java
@@ -1,20 +1,33 @@
 package dev.codenation.errorcentral.endpoints;
 
 import dev.codenation.errorcentral.entity.Event;
+import dev.codenation.errorcentral.exceptions.ResourceNotFoundException;
 import dev.codenation.errorcentral.service.impl.EventService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/v1/eventlog")
+@RequestMapping("/v1/api/event")
 public class EventController {
 
-    private EventService eventService;
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping
+    public List<Event> findAll() {
+        return this.eventService.findAll();
+    }
 
     @GetMapping("/{id}")
-    public Event findById(@PathVariable Long id) {
-        return eventService.findById(id).get();
+    public ResponseEntity<Event> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(eventService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Event")));
     }
 }
