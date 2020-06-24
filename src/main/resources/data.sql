6316dea3c0daf8781518411b99a89ff8aa23b054
Rafael Borges <rafaelfborges@gmail.com>
2020-06-24 19:46:19 -0300

    Insert more data to test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,8 @@ INSERT INTO events VALUES (2, {ts '2012-09-17 18:47:52.69'}, 'System failure at 
 INSERT INTO events VALUES (3, {ts '2012-09-17 18:48:5.69'}, 'System failure at boot', 'WARNING', 'The system fails at boot with error message xyz', 1, 'VM Web Server');
 INSERT INTO events VALUES (4, {ts '2012-09-17 18:49:10.69'}, 'System failure at boot', 'INFO', 'The system fails at boot with error message xyz', 1, 'VM Testing Application Server');
 INSERT INTO events VALUES (5, {ts '2012-09-17 18:50:09.13'}, 'System failure at boot', 'WARNING', 'The system fails at boot with error message xyz', 1, 'VM Application Server');
+INSERT INTO events VALUES (6, {ts '2013-10-17 18:46:01.69'}, 'Fail providing memory', 'ERROR', 'Pool manager fails to provide system memory for server xyz', 3, 'VM Manager');
+INSERT INTO events VALUES (7, {ts '2013-10-17 18:47:52.69'}, 'System failure at boot', 'WARNING', 'The system fails at boot with error message xyz', 1, 'VM DataBase Server');
+INSERT INTO events VALUES (8, {ts '2013-10-17 18:48:5.69'}, 'System failure at boot', 'WARNING', 'The system fails at boot with error message xyz', 1, 'VM Web Server');
+INSERT INTO events VALUES (9, {ts '2013-10-17 18:49:10.69'}, 'System failure at boot', 'INFO', 'The system fails at boot with error message xyz', 1, 'VM Testing Application Server');
+INSERT INTO events VALUES (10, {ts '2013-10-17 18:50:09.13'}, 'System failure at boot', 'WARNING', 'The system fails at boot with error message xyz', 1, 'VM Application Server');
