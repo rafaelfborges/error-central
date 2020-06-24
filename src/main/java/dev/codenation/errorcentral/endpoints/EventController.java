5971cc18fad52302b7373aa8fa6add4dca823382
Rafael Borges <rafaelfborges@gmail.com>
2020-06-24 04:32:16 -0300

    Add event mappper to return a DTO object

--- src/main/java/dev/codenation/errorcentral/endpoints/EventController.java
+++ src/main/java/dev/codenation/errorcentral/endpoints/EventController.java
@@ -1,29 +1,34 @@
 package dev.codenation.errorcentral.endpoints;
 
+import dev.codenation.errorcentral.dto.EventDTO;
 import dev.codenation.errorcentral.entity.Event;
 import dev.codenation.errorcentral.exceptions.ResourceNotFoundException;
+import dev.codenation.errorcentral.mappers.EventMapper;
 import dev.codenation.errorcentral.service.impl.EventService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/v1/api/event")
 public class EventController {
 
     private final EventService eventService;
+    private final EventMapper eventMapper;
 
-    public EventController(EventService eventService) {
-        this.eventService = eventService;
+    @PostMapping
+    public ResponseEntity<Event> create(@Valid @RequestBody Event event) {
+        return new ResponseEntity<>(this.eventService.save(event), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public List<Event> findAll() {
-        return this.eventService.findAll();
+    public List<EventDTO> findAll() {
+        return eventMapper.map(eventService.findAll());
     }
 
     @GetMapping("/{id}")
