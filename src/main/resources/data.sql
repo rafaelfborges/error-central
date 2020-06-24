1c8bb20901c14e357d6b547fbf8c2c2793c800e6
Rafael Borges <rafaelfborges@gmail.com>
2020-06-24 04:27:04 -0300

    Pre loading H2 database with testing records

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO events VALUES (1, {ts '2012-09-17 18:46:01.69'}, 'Fail providing memory', 'ERROR', 'Pool manager fails to provide system memory for server xyz', 3, 'VM Manager');
+INSERT INTO events VALUES (2, {ts '2012-09-17 18:47:52.69'}, 'System failure at boot', 'WARNING', 'The system fails at boot with error message xyz', 1, 'VM DataBase Server');
+INSERT INTO events VALUES (3, {ts '2012-09-17 18:48:5.69'}, 'System failure at boot', 'WARNING', 'The system fails at boot with error message xyz', 1, 'VM Web Server');
+INSERT INTO events VALUES (4, {ts '2012-09-17 18:49:10.69'}, 'System failure at boot', 'INFO', 'The system fails at boot with error message xyz', 1, 'VM Testing Application Server');
+INSERT INTO events VALUES (5, {ts '2012-09-17 18:50:09.13'}, 'System failure at boot', 'WARNING', 'The system fails at boot with error message xyz', 1, 'VM Application Server');
