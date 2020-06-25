768a4f83228711952cfe4d2787f6602150936ce4
Rafael Borges <rafaelfborges@gmail.com>
2020-06-24 23:15:32 -0300

    Add a admin user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+INSERT INTO users VALUES (1, 'rafaelfborges@gmail.com', 'Rafael Borges', '123456');
 INSERT INTO events VALUES (1, {ts '2012-09-17 18:46:01.69'}, 'Fail providing memory', 'ERROR', 'Pool manager fails to provide system memory for server xyz', 3, 'VM Manager');
 INSERT INTO events VALUES (2, {ts '2012-09-17 18:47:52.69'}, 'System failure at boot', 'WARNING', 'The system fails at boot with error message xyz', 1, 'VM DataBase Server');
 INSERT INTO events VALUES (3, {ts '2012-09-17 18:48:5.69'}, 'System failure at boot', 'WARNING', 'The system fails at boot with error message xyz', 1, 'VM Web Server');
